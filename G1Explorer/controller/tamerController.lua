3fa300627ba863ed0296fbe16ee4166ccc308615
AriAlanPR <shaymin2003@gmail.com>
2019-10-30 00:57:52 -0500

    added some new functions and adapted a new project G1Explorer to mvc

--- G1Explorer/controller/tamerController.lua
+++ G1Explorer/controller/tamerController.lua
@@ -1,24 +1,51 @@
 -- Load required libraries
 local physics = require( "physics" )
-local tamer = require("model.tamer")
+local tamer = require("model.characters.tamer")
 
 local tamerController = {}
 
 -- Configure image sheets
 local tamerArray = tamer:getSheet()
 local tamerOptions = tamerArray
-local tamerSheet = graphics.newImageSheet( "assets/images/character1.png", tamerOptions )
+local tamerSheet = graphics.newImageSheet( "assets/images/tamer.png", tamerOptions )
 
-local charSequence = {
-    {name="stand", start=123, count=11, time=600, loopCount },
-    {name="run", start=70, count=8, time=600 },
-    {name="jump", start=88, count=8, time=600, loopCount=1 }
+local tamerSequence = {
+    {name="stand", start=1, count=5, time=600},
+    {name="run", start=6, count=1, time=600 },
+    {name="jump", start=7, count=6, time=600, loopCount=1 }
 }
 
-tamerController.sprite = display.newSprite( mainGroup, charSheet, charSequence)
+function tamerController:init(targetGroup)
+-- print("Tamer group: ", targetGroup)
+tamerController.sprite = display.newSprite( targetGroup, tamerSheet, tamerSequence)
+tamerController.sprite:scale( 2, 2 )
+tamerController.sprite.myName = "tamer"
+tamerController.body = physics.addBody( tamerController.sprite, "dynamic", { density=1.5, friction=0.3, shape={-10,-25 ,10,-25 ,10,30 ,-10,30}, isBullet=true } )
+
+-- Play character animation
+tamerController.sprite:setSequence( "jump" )
+tamerController.sprite:play()
+end
 
 function tamerController:getSprite()
     return self.sprite;
+end
+
+function tamerController:getBody()
+    return self.body;
+end
+ 
+tamerController.onCollision = function ( event )
+    if ( event.phase == "began" ) then
+        local o1 = event.object1
+        local o2 = event.object2
+        
+        if((o1.myName == "tamer" and o2.myName == "crate") or (o1.myName == "crate" and o2.myName == "tamer")) then 
+            -- Play character stand
+            tamerController.sprite:setSequence( "stand" )
+            tamerController.sprite:play()
+        end
+    end
 end
 
 return tamerController
--- G1Explorer/view/level1.lua
+++ G1Explorer/view/level1.lua
@@ -9,11 +9,13 @@ local scene = composer.newScene()
 
 -- include Corona's "physics" library
 local physics = require "physics"
-
+local tamerController = require "controller.tamerController"
 --------------------------------------------
 
 -- forward declarations and other locals
 local screenW, screenH, halfW, halfH = display.actualContentWidth, display.actualContentHeight, display.contentCenterX, display.contentCenterY
+-- define groups
+local backgroup, maingroup, uigroup
 
 function scene:create( event )
 
@@ -23,47 +25,67 @@ function scene:create( event )
 	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
 
 	local sceneGroup = self.view
+	
+	-- all display objects must be inserted into group
+	backgroup = display.newGroup() 
+	sceneGroup:insert( backgroup )
+
+	maingroup = display.newGroup() 
+	sceneGroup:insert( maingroup)
+	
+	uigroup = display.newGroup() 
+	sceneGroup:insert( uigroup )	
 
 	-- We need physics started to add bodies, but we don't want the simulaton
 	-- running until the scene is on the screen.
 	physics.start()
-	physics.setGravity( -9, 0 )
+	-- physics.setGravity( -9, 0 )
 	physics.pause()
-
-
+	
 	-- create a grey rectangle as the backdrop
 	-- the physical screen will likely be a different shape than our defined content area
 	-- since we are going to position the background from it's top, left corner, draw the
 	-- background at the real top, left corner.
-	local background = display.newRect( display.screenOriginX, display.screenOriginY, screenW, screenH )
+	local background = display.newRect(backgroup, display.screenOriginX, display.screenOriginY, screenW, screenH )
 	background.anchorX = 0 
 	background.anchorY = 0
 	background:setFillColor( .5 )
 	
 	-- make a crate (off-screen), position it, and rotate slightly
-	local crate = display.newImageRect( "crate.png", 90, 90 )
-	crate.x, crate.y = display.contentCenterX, halfH
-	crate.rotation = 8
+	local crate = display.newImageRect(maingroup, "crate.png", 90, 90 )
+	crate.x, crate.y = halfW, halfH + 110 
+	crate.myName = "crate"
+	crate.rotation = 5
 	
 	-- add physics to the crate
-	physics.addBody( crate, "dynamic", { density=1.0, friction=0.3, bounce=0.3 } )
+	physics.addBody( crate, "dynamic", { density=1.0, friction=0.3, bounce=0.2 } )
 	
+	-- make a crate (off-screen), position it, and rotate slightly
+	local crate2 = display.newImageRect(maingroup, "crate.png", 90, 90 )
+	crate2.x, crate2.y = screenW - 100, halfH 
+	crate2.myName = "crate"
+	crate2.rotation = 8
+	
+	-- add physics to the crate2
+	physics.addBody( crate2, "dynamic", { density=1.0, friction=0.3, bounce=0.2 } )
+
 	-- create a grass object and add physics (with custom shape)
-	local grass = display.newImageRect( "grass.png", screenH, 82 )
+	local grass = display.newImageRect(maingroup, "grass.png", screenW, 99 )
 	grass.anchorX = 0
 	grass.anchorY = 1
-	grass.rotation = 90
+	-- grass.rotation = 90
 	--  draw the grass at the very bottom of the screen
-	grass.x, grass.y = 0, 0 + display.screenOriginY
+	grass.x, grass.y = 0, screenH + (display.screenOriginY * 2)
 	
 	-- define a shape that's slightly shorter than image bounds (set draw mode to "hybrid" or "debug" to see)
-	local grassShape = { -halfH,-34, halfH,-34, halfH,34, -halfH,34 }
+	local grassShape = { -halfW,-34, halfW,-34, halfW,34, -halfW,34 }
 	physics.addBody( grass, "static", { friction=0.3, shape=grassShape } )
 	
-	-- all display objects must be inserted into group
-	sceneGroup:insert( background )
-	sceneGroup:insert( grass)
-	sceneGroup:insert( crate )
+	tamerController:init(maingroup)
+	local tamerObj = tamerController:getSprite()
+	tamerObj.x = halfW
+	tamerObj.y = halfH
+	
 end
 
 
@@ -79,6 +101,7 @@ function scene:show( event )
 		-- INSERT code here to make the scene come alive
 		-- e.g. start timers, begin animation, play audio, etc.
 		physics.start()
+		Runtime:addEventListener( "collision", tamerController.onCollision )
 	end
 end
 
@@ -95,6 +118,7 @@ function scene:hide( event )
 		physics.stop()
 	elseif phase == "did" then
 		-- Called when the scene is now off screen
+		Runtime:removeEventListener( "collision", tamerController.onCollision )
 	end	
 	
 end
